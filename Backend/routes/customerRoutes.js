5c53b71e89cd06233095a69434a96213017b9dcb
Manish Chhetri <chhetridon123@gmail.com>
2022-07-11 01:05:34 +0545

    Adding routes

--- Backend/routes/customerRoutes.js
+++ Backend/routes/customerRoutes.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const customerController = require("../controller/customerController");
 router.get("/customer", customerController.getAllCustomers);
+router.post("/customer", customerController.postCustomers);
 
 module.exports = router;
